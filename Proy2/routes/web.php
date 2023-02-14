3574a648e969089aa333ccfc0d3c739e56100377
Nestor181 <nestorfm801@gmail.com>
2023-02-14 16:42:30 -0600

    Creando 2do proyecto para aprender livewire Of.

--- Proy2/routes/web.php
+++ Proy2/routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Livewire\ShowPosts;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
@@ -22,7 +22,5 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', ShowPosts::class)->name('dashboard');
 });
