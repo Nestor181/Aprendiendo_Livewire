c93d94467cfd123bf3a3c7f2a7bc608103cd94fe
E_n_i <nestorfm801@gmail.com>
2023-02-15 12:38:59 -0600

    Tercer intento para ver que funcione wire:model

--- livewire/resources/views/livewire/show-posts.blade.php
+++ livewire/resources/views/livewire/show-posts.blade.php
@@ -4,7 +4,7 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-    <h1>{{$search}}</h1>
+   {{-- <h1>{{$search}}</h1>--}}
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <!---Table---->
        <x-table>
